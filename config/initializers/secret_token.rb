9ff333f43f57e735b57e899413c1068ceb3a0afb
Alvaro Sendon Aleu <alvaro@alsenda.com>
2012-03-28 00:58:36 +0200

    BLemon initial commit. Nothing to see here yet.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blemon::Application.config.secret_token = '811ac3c5eb013569862bcd070e686de8e22ca54dd527e54d3625ceafd0303221160ed4b32462a3faf0fbfef81a89ca517d1d160f43f98c258ac06b651b1ca3b5'
